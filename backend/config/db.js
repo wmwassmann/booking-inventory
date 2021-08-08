40f598779cbc017a7434d9a67888f12495b040a2
William Wassmann <wmwassmann@hotmail.com>
2021-08-08 14:05:48 -0700

    Maybe this time I will win

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {
+        await mongoose.connect(process.env.ATLAS_URI, {
             useNewUrlParser: true,
             useFindAndModify: true,
             useUnifiedTopology: true,
